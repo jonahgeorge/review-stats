b69bb1505b99ff24d502af7542e8f5db8547733c
Jonah George <jonah.george@icloud.com>
2023-12-18 13:51:38 -0800

    Various refactors
    - Paginates PRs to fetch all
    - Finds PRs in all states (not just open)
    - Displays a table of results
    - Display progress bars

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"context"
 	"flag"
+	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/google/go-github/v57/github"
 	"github.com/montanaflynn/stats"
 	"github.com/olekukonko/tablewriter"
+	"github.com/schollz/progressbar/v3"
 )
 
 var (
@@ -35,13 +38,96 @@ func main() {
 
 	times, err := reviewStats.GetReviewTimes(context.Background())
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Name", "p25", "p50 (Median)", "p75"})
+	reviewStats.PrintTable(times)
+}
 
-	for user, durations := range times {
+type Stats struct {
+	Owner  string
+	Repo   string
+	Client *github.Client
+}
+
+func (s *Stats) getPullRequests(ctx context.Context) ([]*github.PullRequest, error) {
+	var allPulls []*github.PullRequest
+
+	opt := &github.PullRequestListOptions{
+		State:       "all",
+		ListOptions: github.ListOptions{PerPage: 100},
+	}
+
+	bar := progressbar.NewOptions(
+		-1,
+		progressbar.OptionSetDescription("Fetching pull requests..."),
+	)
+	for {
+		bar.Add(1)
+
+		pulls, resp, err := s.Client.PullRequests.List(ctx, s.Owner, s.Repo, opt)
+		if err != nil {
+			return nil, err
+		}
+
+		allPulls = append(allPulls, pulls...)
+		if resp.NextPage == 0 {
+			break
+		}
+
+		opt.Page = resp.NextPage
+	}
+
+	return allPulls, nil
+}
+
+func (s *Stats) getReviews(ctx context.Context, pull_number int) ([]*github.PullRequestReview, *github.Response, error) {
+	return s.Client.PullRequests.ListReviews(ctx, s.Owner, s.Repo, pull_number, nil)
+}
+
+func (s *Stats) GetReviewTimes(ctx context.Context) (map[string][]float64, error) {
+	reviewTimes := make(map[string][]float64)
+
+	pulls, err := s.getPullRequests(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	bar := progressbar.NewOptions(
+		len(pulls),
+		progressbar.OptionSetDescription("Fetching reviews..."),
+	)
+	for _, pull := range pulls {
+		bar.Add(1)
+
+		reviews, _, err := s.getReviews(ctx, *pull.Number)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, review := range reviews {
+			times, ok := reviewTimes[*review.User.Login]
+			if !ok {
+				times = make([]float64, 0)
+			}
+
+			if review.SubmittedAt != nil {
+				reviewTimes[*review.User.Login] = append(
+					times,
+					review.SubmittedAt.Sub(pull.CreatedAt.Time).Seconds(),
+				)
+			}
+		}
+	}
+
+	return reviewTimes, nil
+}
+
+func (s *Stats) PrintTable(timing map[string][]float64) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "p25", "p50 (Median)", "p75", "Count"})
+
+	for user, durations := range timing {
 		data := stats.LoadRawData(durations)
 
 		p25, _ := stats.Percentile(data, 25)
@@ -53,50 +139,9 @@ func main() {
 			(time.Duration(p25) * time.Second).String(),
 			(time.Duration(p50) * time.Second).String(),
 			(time.Duration(p75) * time.Second).String(),
+			strconv.Itoa(len(data)),
 		})
 	}
 
 	table.Render()
-}
-
-type Stats struct {
-	Owner  string
-	Repo   string
-	Client *github.Client
-}
-
-func (s *Stats) GetPullRequests(ctx context.Context) ([]*github.PullRequest, *github.Response, error) {
-	return s.Client.PullRequests.List(ctx, s.Owner, s.Repo, &github.PullRequestListOptions{
-		ListOptions: github.ListOptions{PerPage: 1000},
-	})
-}
-
-func (s *Stats) GetReviews(ctx context.Context, pull_number int) ([]*github.PullRequestReview, *github.Response, error) {
-	return s.Client.PullRequests.ListReviews(ctx, s.Owner, s.Repo, pull_number, nil)
-}
-
-func (s *Stats) GetReviewTimes(ctx context.Context) (map[string][]float64, error) {
-	reviewTimes := make(map[string][]float64)
-
-	pulls, _, err := s.GetPullRequests(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, pull := range pulls {
-		reviews, _, err := s.GetReviews(ctx, *pull.Number)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, review := range reviews {
-			times, ok := reviewTimes[*review.User.Login]
-			if !ok {
-				times = make([]float64, 0)
-			}
-			reviewTimes[*review.User.Login] = append(times, review.SubmittedAt.Sub(pull.CreatedAt.Time).Seconds())
-		}
-	}
-
-	return reviewTimes, nil
 }
